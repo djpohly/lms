db8edf2606ceeb25af8e04984b5655e5c5c2c675
Devin J. Pohly <djpohly@gmail.com>
2019-12-14 23:35:01 -0600

    make linter happy

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-from schoology import *
+from schoology import Schoology
 import click
 from click_repl import repl
 import configparser
 import xdg
-import sys
 
 CONFIG_FILE = xdg.XDG_CONFIG_HOME/'lms.conf'
 conf = configparser.ConfigParser(default_section=None)
 conf.read(CONFIG_FILE)
 be = Schoology(conf['schoology'])
 
+
 @click.group(invoke_without_command=True)
 @click.pass_context
 def cli(ctx):
     if ctx.invoked_subcommand is None:
-        repl(ctx, allow_system_commands=False, prompt_kwargs={'completer': None})
+        repl(ctx, allow_system_commands=False,
+                prompt_kwargs={'completer': None})
+
 
 @cli.command()
 def config():
@@ -22,20 +24,24 @@ def config():
         for k, v in conf[s].items():
             click.echo(f'{s}.{k} = {v}')
 
+
 @cli.command()
 def schools():
     for school in be.schools:
         click.echo(school)
+
 
 @cli.command()
 def me():
     gotta = be.me
     click.echo(gotta)
 
+
 @cli.command()
 def langs():
     for code, lang in be.languages.items():
         click.echo(f'{code}\t {lang}')
+
 
 @cli.command()
 def sections():
