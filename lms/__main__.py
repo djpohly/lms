3ca14f48d4db6ebdf3a3263ab55c2878a36a4cbb
Devin J. Pohly <djpohly@gmail.com>
2019-12-17 15:01:02 -0600

    rename cli to main

--- lms/__main__.py
+++ lms/__main__.py
@@ -22,44 +22,44 @@ cur_course = None
 @click.group(invoke_without_command=True)
 @click_log.simple_verbosity_option(log)
 @click.pass_context
-def cli(ctx):
+def main(ctx):
     if ctx.invoked_subcommand is None:
         repl(ctx, allow_system_commands=False,
                 prompt_kwargs={'completer': None})
 
 
-@cli.command()
+@main.command()
 def config():
     for s in conf.sections():
         for k, v in conf[s].items():
             click.echo(f'{s}.{k} = {v}')
 
 
-@cli.command()
+@main.command()
 def schools():
     for school in be.schools:
         click.echo(school)
 
 
-@cli.command()
+@main.command()
 def me():
     gotta = be.me
     click.echo(gotta)
 
 
-@cli.command()
+@main.command()
 def langs():
     for code, lang in be.languages.items():
         click.echo(f'{code}\t {lang}')
 
 
-@cli.command()
+@main.command()
 def sections():
     for sec in be.me.sections:
         click.echo(sec)
 
 
-@cli.command()
+@main.command()
 @click.argument('search', default='')
 def courses(search):
     global cur_course
@@ -71,8 +71,11 @@ def courses(search):
                 click.echo(f' {n:2}. {c}')
 
 
-@cli.command()
+@main.command()
 @click.argument('num')
 def course(num):
     global cur_course
     cur_course = be.me.courses[int(num)]
+
+if __name__ == '__main__':
+    main()
