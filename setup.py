2d13dc11d8da45a179036b6740eda7fd7ffa448d
Devin J. Pohly <djpohly@gmail.com>
2019-12-17 14:33:48 -0600

    migrate to setup.cfg

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='lms',
-    version='0.1',
-    py_modules=['main'],
-    install_requires=[
-        'Click',
-        'click-repl',
-        'click-log',
-        'schoolopy',
-        'xdg',
-        'cached_property',
-    ],
-    entry_points='''
-        [console_scripts]
-        lms=main:cli
-    ''',
-)
+setuptools.setup()
